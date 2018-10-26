f01fe5f0db2cdc4556557d55cda53e8279de7a42
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-10-26 11:26:43 +0100

    Added GetData - all data coming through

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const Films = require("./models/films.js");
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("JavaScript Loaded");
+
+  const films = new Films();
+  films.getData();
+
+});
---
+++ src/models/films.js
@@ -0,0 +1,30 @@
+const Request = require("../helpers/request.js");
+const PubSub = require("../helpers/pub_sub.js");
+
+const Films = function() {
+  this.filmData = null;
+};
+
+Films.prototype.getData = function () {
+  const url = "https://ghibliapi.herokuapp.com/films";
+  const request = new Request(url);
+  request.get()
+    .then((data) => {
+     this.filmData = data;
+     //PubSub.publish("Films:all-film-data", this.filmData);
+     console.log(this.filmData);
+    })
+   .catch((error) => {
+      //PubSub.publish("Films:error", error);
+    })
+};
+
+// Films.prototype.bindEvents = function () {
+//
+// };
+
+// Films.prototype.publishFilmDetails = function () {
+//
+// };
+
+module.exports = Films;
