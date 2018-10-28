310c4962536efc148da3e67c703ec62846699052
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-10-28 12:42:23 +0000

    Refactored FilmInfo code

--- src/views/film_info.js
+++ src/views/film_info.js
@@ -3,8 +3,8 @@ const FilmInfo = function (container, film) {
   this.film = film;
 };
 
+//Full Display:
 FilmInfo.prototype.display = function () {
-
   const infoDiv = document.createElement("div");
   infoDiv.classList.add("info-div");
 
@@ -14,13 +14,13 @@ FilmInfo.prototype.display = function () {
   const filmDescription = this.createDescription();
   infoDiv.appendChild(filmDescription);
 
-  const filmList = this.createList();
-  infoDiv.appendChild(filmList);
+  const filmDetails = this.createDetails();
+  infoDiv.appendChild(filmDetails);
 
   this.container.appendChild(infoDiv);
 };
 
-
+//Title:
 FilmInfo.prototype.createTitle = function () {
   const title = document.createElement("h2");
   title.textContent = this.film.title;
@@ -28,6 +28,7 @@ FilmInfo.prototype.createTitle = function () {
   return title;
 };
 
+//Description:
 FilmInfo.prototype.createDescription = function () {
   const description = document.createElement("p");
   description.textContent = this.film.description;
@@ -35,34 +36,65 @@ FilmInfo.prototype.createDescription = function () {
   return description;
 };
 
-FilmInfo.prototype.createList = function () {
-  const detailsList = document.createElement("div");
+//Other Details:
+FilmInfo.prototype.createDetails = function () {
+  const detailsDiv = document.createElement("div");
+
+  const director = this.createDirector();
+  detailsDiv.appendChild(director);
+
+  const releaseDate = this.createReleaseDate();
+  detailsDiv.appendChild(releaseDate);
+
+  const rating = this.createRating();
+  detailsDiv.appendChild(rating);
+
+  return detailsDiv;
+};
+
+//Director:
+FilmInfo.prototype.createDirector = function () {
+  const directorDiv = document.createElement("div");
 
   const directorLabel = document.createElement("h4");
   directorLabel.textContent = "Director:";
-  detailsList.appendChild(directorLabel);
+  directorDiv.appendChild(directorLabel);
 
   const director = document.createElement("p");
   director.textContent = this.film.director;
-  detailsList.appendChild(director);
+  directorDiv.appendChild(director);
+
+  return directorDiv;
+};
+
+//Release Date:
+FilmInfo.prototype.createReleaseDate = function () {
+  const releaseDiv = document.createElement("div");
 
   const releaseLabel = document.createElement("h4");
   releaseLabel.textContent = "Release Year:";
-  detailsList.appendChild(releaseLabel);
+  releaseDiv.appendChild(releaseLabel);
 
   const releaseDate = document.createElement("p");
   releaseDate.textContent = this.film.release_date;
-  detailsList.appendChild(releaseDate);
+  releaseDiv.appendChild(releaseDate);
+
+  return releaseDiv;
+};
+
+//Rating:
+FilmInfo.prototype.createRating = function () {
+  const ratingDiv = document.createElement("div");
 
   const ratingLabel = document.createElement("h4");
   ratingLabel.textContent = "Rating:";
-  detailsList.appendChild(ratingLabel);
+  ratingDiv.appendChild(ratingLabel);
 
   const rating = document.createElement("p");
   rating.textContent = this.film.rt_score;
-  detailsList.appendChild(rating);
+  ratingDiv.appendChild(rating);
 
-  return detailsList;
+  return ratingDiv;
 };
 
 module.exports = FilmInfo;
